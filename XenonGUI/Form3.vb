41c4b1ec01d168b7660550af99fb639501e776ce
Esquire-2000 <stevebobygeorge@gmail.com>
2020-11-27 22:10:37 +0530

    Password for encryption/decrytion form comments

--- XenonGUI/Form3.vb
+++ XenonGUI/Form3.vb
@@ -1,13 +1,13 @@
-﻿Public Class Form3
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.Hide()
+﻿'This is the Password for encryption/decryption form (Form 3)
+'------------------
+'Password means Password input
+'Button1 means Confirm button
+'sender As Object means parameter that contains the reference to the control/object that raised the event
+'e As Event means e contains the event data
+'------------------
+Public Class Form3 'Class Form 3
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click 'subroutine when confirm button is clicked
+        Me.Hide() 'hide Password for encryption/decryption form
     End Sub
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
-
-    End Sub
-
-
-
 
 End Class
